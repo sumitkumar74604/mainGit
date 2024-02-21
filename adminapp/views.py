089e788d639aaebc7fb3e2776c1899ef7a1344bb
sumitkumar74604 <152770548+sumitkumar74604@users.noreply.github.com>
2024-02-21 12:05:28 +0530

    Update views.py

--- adminapp/views.py
+++ adminapp/views.py
@@ -167,7 +167,7 @@ def viewcategory(request):
 
 def Logout(request):
     logout(request)
-    return redirect('http://13.213.4.203:8004/login')
+    return redirect('http://13.213.4.203:8001/login')
 
 #django rest framework
 '''
